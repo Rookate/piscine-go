806aaa0a781833014cfd539829450bf33a2f9c96
gkopoin <gab270b@gmail.com>
2024-02-26 14:31:52 +0100

    to-git-or-not-to-git

--- to-git-or-not-to-git.sh
+++ to-git-or-not-to-git.sh
@@ -1,1 +1,3 @@
-printf "Chameleon\n28\nMale"
+curl --silent "https://zone01normandie.org/assets/superhero/all.json" | jq -r '.[] | select(.id==170) | .name '
+curl --silent "https://zone01normandie.org/assets/superhero/all.json" | jq -r '.[] | select(.id==170) | .powerstats.power '
+curl --silent "https://zone01normandie.org/assets/superhero/all.json" | jq -r '.[] | select(.id==170) | .appearance.gender '

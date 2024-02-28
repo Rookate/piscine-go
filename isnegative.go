f31232c9eb852a2bd86b93e6bcb23784c238ecd9
gkopoin <gab270b@gmail.com>
2024-02-28 13:22:58 +0100

    isnegative

--- isnegative.go
+++ isnegative.go
@@ -1,18 +1,14 @@
-package psicine
+package piscine
 
-import ("github.com/01-edu/z01")
-
+import (
+    "github.com/01-edu/z01"
+)
 
 func IsNegative(nb int) {
-
-	if nb < 0 (
-		z01.PrintRune('T')
-	)
-
-	else {
-		
-		z01.PrintRune('F')
-	}
-
-	z01.PrintRune('\n')
+    if nb < 0 {
+        z01.PrintRune('T')
+    } else {
+        z01.PrintRune('F')
+    }
+    z01.PrintRune('\n')
 }

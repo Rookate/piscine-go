b2ecaa2bce1eae4991d7cc5be89b0eb859efa6dd
gkopoin <gab270b@gmail.com>
2024-02-28 20:14:42 +0100

    printcomb2

--- printcomb.go
+++ printcomb.go
@@ -1,21 +1,25 @@
 package piscine
 
-import("github.com/01-edu/z01")
+import (
+	"github.com/01-edu/z01"
+)
 
 // boucle forte
 
-func PrintComb(){    
-    for x := '0'; x<= '7'; x++ {
-      for y := x+1; y<= '8'; y++ {
-         for z := y+1; z<= '9'; z++ {
-        z01.PrintRune(x) 
-        z01.PrintRune(y)
-        z01.PrintRune(z)
-           if x < '7' {
-             z01.PrintRune(',') // afficher des virgules
-        z01.PrintRune(' ') // afficher des espace 
-           }
-    }}}
-       z01.PrintRune('\n') // afficher en ligne 
+func PrintComb() {
+	for x := '0'; x <= '7'; x++ {
+		for y := x + 1; y <= '8'; y++ {
+			for z := y + 1; z <= '9'; z++ {
+				z01.PrintRune(x)
+				z01.PrintRune(y)
+				z01.PrintRune(z)
+				if x < '7' {
+					z01.PrintRune(',') // afficher des virgules
+					z01.PrintRune(' ') // afficher des espace
+				}
+			}
+		}
+	}
+	z01.PrintRune('\n') // afficher en ligne
 
 }

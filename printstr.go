7c57025985dfca507a4c042b5704c2ff4b23c394
gkopoin <gab270b@gmail.com>
2024-02-29 13:00:43 +0100

    printstr

---
+++ printstr.go
@@ -0,0 +1,12 @@
+package piscine
+
+import (
+    "github.com/01-edu/z01"
+)
+
+func PrintStr(s string) {
+    for _, s := range s { // Parcours chaque caractère de la chaîne
+        z01.PrintRune(s) // Affiche le caractère
+    }
+}
+

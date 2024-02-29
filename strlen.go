cdc8d831dbfd9d3576f2a896b3ea92af203fa845
gkopoin <gab270b@gmail.com>
2024-02-29 13:55:46 +0100

    swap.go

--- strlen.go
+++ strlen.go
@@ -1,9 +1,9 @@
-package piscine	
+package piscine
 
 func StrLen(s string) int {
 	count := 0
-  for range s {
-      count ++ 
-    }
-  return count
+	for range s {
+		count++
+	}
+	return count
 }

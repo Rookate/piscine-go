a0dfcb8811aed98cef993f3d9832da1eaf9ed3e3
gkopoin <gab270b@gmail.com>
2024-02-29 13:04:03 +0100

    printstr.go

--- printstr.go
+++ printstr.go
@@ -1,12 +1,11 @@
 package piscine
 
 import (
-    "github.com/01-edu/z01"
+	"github.com/01-edu/z01"
 )
 
 func PrintStr(s string) {
-    for _, s := range s { // Parcours chaque caractère de la chaîne
-        z01.PrintRune(s) // Affiche le caractère
-    }
+	for _, s := range s { // Parcours chaque caractère de la chaîne
+		z01.PrintRune(s) // Affiche le caractère
+	}
 }
-

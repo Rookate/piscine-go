5b36a1cee21f7c5d061e0314d6c0d090514d7b21
gkopoin <gab270b@gmail.com>
2024-03-04 09:52:16 +0100

    iterativefactorial.go

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,13 +4,11 @@ func IterativeFactorial(nb int) int {
 	if nb < 0 || nb > 12 {
 		return 0
 	}
-
 	if nb == 0 || nb == 1 {
 		return 1
 
 	}
 	result := 0
-
 	for i := 0; i <= nb+1; i++ {
 		result = result + nb
 	}

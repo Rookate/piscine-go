5666ed5a9487b68f43a439dfa28d6034cc177687
gkopoin <gab270b@gmail.com>
2024-03-04 13:16:43 +0100

    recursivepower.go

--- recursivepower.go
+++ recursivepower.go
@@ -1,10 +1,7 @@
 package piscine
 
 func RecursivePower(nb int, power int) int {
-	if nb < 0 || nb > 20 {
-		return 0
-	}
-	if power < 0 || power > 20 {
+	if power < 0 {
 		return 0
 	}
 	if power == 0 {

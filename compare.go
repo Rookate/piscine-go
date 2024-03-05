787b9e0c7b4f370c05b496c9fc1a434243666407
gkopoin <gab270b@gmail.com>
2024-03-05 11:05:37 +0100

    alphacount.go

--- compare.go
+++ compare.go
@@ -1,14 +1,14 @@
-package piscine 
+package piscine
 
 func Compare(a, b string) int {
-  if a == b {
-  return 0 
-  } 
-  if a < b {
-  return -1 
-  } 
-  if a > b {
-  return 1
-  }
-  return 0
+	if a == b {
+		return 0
+	}
+	if a < b {
+		return -1
+	}
+	if a > b {
+		return 1
+	}
+	return 0
 }

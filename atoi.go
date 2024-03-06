60736fd6234e4213e9fc9bd69b3dd3e085eec334
gkopoin <gab270b@gmail.com>
2024-03-06 11:14:09 +0100

    atoi.go

---
+++ atoi.go
@@ -0,0 +1,18 @@
+package piscine
+
+func Atoi(s string) int {
+	result := 0
+	p := 1
+	for i, char := range s {
+		if i == 0 && (char == '+' || char == '-') {
+			if char == '-' {
+				p = -1
+			}
+		} else if char < '0' || char > '9' {
+			return 0
+		} else {
+			result = result*10 + int(char-'0')
+		}
+	}
+	return result * p
+}

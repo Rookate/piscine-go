f864140fb5ad159328b2cdf8de9790e063a107d7
gkopoin <gab270b@gmail.com>
2024-03-07 12:20:03 +0100

    splitwhitespaces.go

---
+++ splitwhitespaces.go
@@ -0,0 +1,16 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+	var tab []string
+	result := -1
+	for i, ch := range s {
+		if ch == ' ' || ch == '\t' || ch == '\n' {
+			if len(s[result+1:i]) > 0 {
+				tab = append(tab, s[result+1:i])
+				result = i
+			}
+		}
+	}
+	tab = append(tab, s[result+1:])
+	return tab
+}

a6a60b4e66aab3ca1db62522a1b7cbbbc9c32e98
root <dev@01-edu.org>
2024-03-07 16:49:09 +0000

    Update 'atoi.go'

--- atoi.go
+++ atoi.go
@@ -4,8 +4,10 @@ func Atoi(s string) int {
 	result := 0
 	p := 1
 	for i, char := range s {
-		if i == 0 && char == '-' {
-			p = -1
+		if i == 0 && (char == '-' || char == '+') {
+			if char == '-' {
+				p = -1
+			}
 		} else if char < '0' || char > '9' {
 			return 0
 		} else {

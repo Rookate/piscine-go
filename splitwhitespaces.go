82aa68bd3943d9bd518c9b3b295d1d9c9731b247
gkopoin <gab270b@gmail.com>
2024-03-07 13:05:35 +0100

    splitwhitespaces.go

--- splitwhitespaces.go
+++ splitwhitespaces.go
@@ -4,9 +4,9 @@ func SplitWhiteSpaces(s string) []string {
 	var tab []string
 	result := -1
 	for i, ch := range s {
-		if ch == ' ' || ch == '\t' || ch == '\n' {
+		if ch == ' ' || ch == '	' || ch == '\n' {
 			if len(s[result+1:i]) > 0 {
-				if s[result+1:i] != " " {
+				if s[result+1:i] != "" {
 					tab = append(tab, s[result+1:i])
 				}
 				result = i

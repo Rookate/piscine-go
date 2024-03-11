7322811fa9532269d3cc02c1fe57029c062e5a25
gkopoin <gab270b@gmail.com>
2024-03-11 12:18:22 +0100

    displayfile

--- displayfile/main.go
+++ displayfile/main.go
@@ -8,20 +8,22 @@ import (
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("File name missing")
+		return
 	}
 	if len(os.Args) > 2 {
 		fmt.Println("Too many arguments")
+		return
 	}
 	fileName := os.Args[1]
 	file, err := os.Open(fileName)
 	if err != nil {
-		fmt.Printf("File name missing")
+		fmt.Println(err.Error())
 		return
 	}
 	arr := make([]byte, 1024)
 	_, err = file.Read(arr)
 	if err != nil {
-		fmt.Printf("File name missing")
+		fmt.Println(err.Error())
 		return
 	}
 	fmt.Print(string(arr))

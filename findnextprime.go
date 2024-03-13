1847d1857efed3b3324bd7ac4607401d9b943745
gkopoin <gab270b@gmail.com>
2024-03-13 14:26:52 +0100

    suite

--- findnextprime.go
+++ findnextprime.go
@@ -3,9 +3,9 @@ package piscine
 func FindNextPrime(nb int) int {
 	answere := 0
 	if nb < 0 {
-		return -1
+		return 2
 	}
-	for i := 2; i <= nb; i++ {
+	for i := nb; i >= nb; i-- {
 		if nb%i == 0 {
 			answere = i
 		}
--- printnbrinorder.go
+++ printnbrinorder.go
@@ -7,32 +7,24 @@ func PrintNbrInOrder(n int) {
 		z01.PrintRune('0')
 		return
 	}
-	if n > 0 {
-		var array []int
-		eachValue := 0
-
-		arrayCount := 0
-		var minValue int
-		for n != 0 {
-			eachValue = n % 10
-			n /= 10
-			array = append(array, eachValue)
-		}
-
-		for count := range array {
-			arrayCount = count + 1
-		}
-		for i := 0; i < arrayCount-1; i++ {
-			for j := 0; j < arrayCount-i-1; j++ {
-				if array[j] > array[j+1] {
-					minValue = array[j]
-					array[j] = array[j+1]
-					array[j+1] = minValue
-				}
+	var array []int
+	eachValue := 0
+	// Conversion de l'entier en un tableau d'entiers
+	for n != 0 {
+		eachValue = n % 10
+		n /= 10
+		array = append(array, eachValue)
+	}
+	// Tri du tableau dans l'ordre croissant
+	for i := 0; i < len(array)-1; i++ {
+		for j := 0; j < len(array)-i-1; j++ {
+			if array[j] > array[j+1] {
+				array[j], array[j+1] = array[j+1], array[j]
 			}
 		}
-		for i := 0; i < arrayCount; i++ {
-			z01.PrintRune(rune(array[i] + 48))
-		}
+	}
+	// Impression des chiffres dans l'ordre croissant
+	for i := 0; i < len(array); i++ {
+		z01.PrintRune(rune(array[i] + '0'))
 	}
 }

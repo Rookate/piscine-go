10f1ee17d197355d2fc20bbeefcdbee0fa207f88
gkopoin <gab270b@gmail.com>
2024-03-14 17:13:59 +0100

    suite

---
+++ enigma.go
@@ -0,0 +1,8 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	***a, *******c = *******c, ***a
+	*b, ***a = ***a, *b
+	*******c, ****d = ****d, *******c
+	****d, *b = *b, ****d
+}
--- test/test.go
+++ test/test.go
@@ -6,6 +6,40 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.FindNextPrime(5))
-	fmt.Println(piscine.FindNextPrime(4))
+	x := 5
+	y := &x
+	z := &y
+	a := &z
+
+	w := 2
+	b := &w
+
+	u := 7
+	e := &u
+	f := &e
+	g := &f
+	h := &g
+	i := &h
+	j := &i
+	c := &j
+
+	k := 6
+	l := &k
+	m := &l
+	n := &m
+	d := &n
+
+	fmt.Println(***a)
+	fmt.Println(*b)
+	fmt.Println(*******c)
+	fmt.Println(****d)
+
+	piscine.Enigma(a, b, c, d)
+
+	fmt.Println("After using Enigma")
+	fmt.Println(***a)
+	fmt.Println(*b)
+	fmt.Println(*******c)
+	fmt.Println(****d)
+
 }

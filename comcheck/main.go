23ffa17c7ddccaeb0ebf654b80eaab6bd380163b
gkopoin <gab270b@gmail.com>
2024-03-14 15:15:24 +0100

    suite

--- comcheck/main.go
+++ comcheck/main.go
@@ -9,7 +9,7 @@ func main() {
 	s := os.Args[1:]
 	for _, ch := range s {
 		if ch == "galaxy" || ch == "01" || ch == "galaxy 01" {
-			fmt.Println("Alert !!!")
+			fmt.Println("Alert!!!")
 			break
 		}
 	}

a04c8e08a32624bb6b8b0be8871926a793855b51
gkopoin <gab270b@gmail.com>
2024-03-14 11:17:03 +0100

    suite

---
+++ abort.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Abort(a, b, c, d, e int) int {
+	var tmp = []int{a, b, c, d, e}
+	for i := 0; i < len(tmp)-1; i++ {
+		for j := 0; j < len(tmp)-i-1; j++ {
+			if tmp[i] < tmp[i+1] {
+				tmp[i], tmp[i+1] = tmp[i+1], tmp[i]
+			}
+		}
+	}
+	return tmp[2]
+}

18d96df4dc21af748f940494265b730eb25944b9
gkopoin <gab270b@gmail.com>
2024-03-15 08:32:36 +0100

    suite

--- issorted.go
+++ issorted.go
@@ -4,20 +4,14 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 	if len(a) <= 1 {
 		return true
 	}
-	if f(a[0], a[1]) < 0 {
-		for k := 0; k < len(a)-1; k++ {
-			if f(a[k], a[k+1]) > 0 {
-				return false
-			}
-		}
-		return true
-	}
-	if f(a[0], a[1]) > 0 {
-		for k := 0; k < len(a)-1; k++ {
-			if f(a[k], a[k+1]) < 0 {
-				return false
-			}
+
+	ascending := f(a[0], a[1]) < 0
+
+	for k := 0; k < len(a)-1; k++ {
+		if (ascending && f(a[k], a[k+1]) > 0) || (!ascending && f(a[k], a[k+1]) < 0) {
+			return false
 		}
 	}
+
 	return true
 }

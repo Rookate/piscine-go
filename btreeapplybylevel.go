c0432244c608da67612d7341cdc15bf4e8f58aeb
gkopoin <gab270b@gmail.com>
2024-03-19 14:16:21 +0100

    suite

--- btreeapplybylevel.go
+++ btreeapplybylevel.go
@@ -4,7 +4,22 @@ func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root == nil {
 		return
 	}
-	f(root.Data)
-	BTreeApplyByLevel(root.Left, f)
-	BTreeApplyByLevel(root.Right, f)
+	tmp := []*TreeNode{root}
+
+	for len(tmp) != 0 {
+		c := tmp[0]
+		tmp = tmp[1:]
+
+		_, err := f(c.Data)
+		if err != nil {
+			return
+		}
+
+		if c.Left != nil {
+			tmp = append(tmp, c.Left)
+		}
+		if c.Right != nil {
+			tmp = append(tmp, c.Right)
+		}
+	}
 }

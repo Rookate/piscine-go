efb9abc33dd23ea9b828fd640ae07683f8cc950f
root <dev@01-edu.org>
2024-03-20 09:07:03 +0000

    Update 'btreedeletenode.go'

--- btreedeletenode.go
+++ btreedeletenode.go
@@ -10,7 +10,7 @@ type TreeNode struct {
 func BTreeDeleteNode(root, node *TreeNode) *TreeNode {
     if root == nil {
         return nil
-    }
+        }
 
     if node.Data < root.Data {
         root.Left = BTreeDeleteNode(root.Left, node)
@@ -42,4 +42,38 @@ func FindMinNode(node *TreeNode) *TreeNode {
         node = node.Left
     }
     return node
+}
+
+func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
+    if root == nil || elem == root.Data {
+        return root
+    }
+    if elem < root.Data {
+        return BTreeSearchItem(root.Left, elem)
+    }
+    return BTreeSearchItem(root.Right, elem)
+}
+
+func BTreeIsBinary(root *TreeNode) bool {
+    if root == nil {
+        return true
+    }
+    if (root.Left != nil && root.Left.Data >= root.Data) || (root.Right != nil && root.Right.Data <= root.Data) { // on check si la branche gauche et droite vérifient bien les conditions de ABR que les enfants soient respectivement inférieurs et superieurs  à la racine
+        return false
+    }
+    return BTreeIsBinary(root.Left) && BTreeIsBinary(root.Right)
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+    if root == nil {
+        return &TreeNode{Data: data}
+    }
+    if data < root.Data {
+        root.Left = BTreeInsertData(root.Left, data)
+        root.Left.Parent = root
+    } else {
+        root.Right = BTreeInsertData(root.Right, data)
+        root.Right.Parent = root
+    }
+    return root
 }

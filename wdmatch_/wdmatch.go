c568b970d6b8ad67966a3a1a36197b88adb3811f
gkopoin <gab270b@gmail.com>
2024-03-28 17:52:04 +0100

    suite

---
+++ wdmatch_/wdmatch.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func CanWrite(first, second string) bool {
+	indexFirst := 0
+
+	for _, ch := range second {
+		if indexFirst < len(first) && byte(ch) == first[indexFirst] {
+			indexFirst++
+		}
+	}
+	return indexFirst == len(first)
+}
+
+func trimQuotes(s string) string {
+	if len(s) >= 2 && s[0] == '"' && s[len(s)-1] == '"' {
+		return s[1 : len(s)-1]
+	}
+	return s
+}
+
+func main() {
+	if len(os.Args) == 3 {
+		firstString := trimQuotes(os.Args[1])  // Retirer les guillemets du premier argument si présents
+		secondString := trimQuotes(os.Args[2]) // Retirer les guillemets du deuxième argument si présents
+		if CanWrite(firstString, secondString) {
+			for _, ch := range firstString {
+				z01.PrintRune(ch)
+			}
+			z01.PrintRune('\n')
+		}
+	}
+}
